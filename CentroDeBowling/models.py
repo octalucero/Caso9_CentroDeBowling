4190562a8a01e4b89dc4662b411c5c19b9b8f2ef
octalucero <octaviolucero2005@gmail.com>
2023-09-01 08:23:02 -0300

    model Client

--- CentroDeBowling/models.py
+++ CentroDeBowling/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Client(models.Model):
+    num_unico = models.IntegerField()
+    nombre = models.CharField(max_length=25)
+    direccion = models.CharField(max_length=75)
+    telefono = models.CharField(max_length=15)
+    correo = models.EmailField(max_length=75)
